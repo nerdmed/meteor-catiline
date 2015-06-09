089169e5209c132706aee3c012179ce545a8305e
Ahmed Hassan <a.hassan@buismed.de>
2015-06-09 17:12:37 +0200

    inital commit

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'flowkey:catiline',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Easy to use web workers library',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('catiline.js', 'client');
+});
